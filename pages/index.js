a0ed9a55b8449a15d1e56f243ec3074fb4a2a233
Akib <akibahmed2011@gmail.com>
2022-02-28 06:45:49 +0600

    homepage complete

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,8 @@ import Header from '../components/Header'
 import Banner from '../components/Banner'
 import SmallCard from '../components/SmallCard'
 import MediumCard from '../components/MediumCard'
+import LargeCard from '../components/LargeCard'
+import Footer from '../components/Footer'
 
 export default function Home({exploreData, cardsData}) {
   return (
@@ -39,9 +41,16 @@ export default function Home({exploreData, cardsData}) {
                 ))}
               </div>
         <section>
-
+            <LargeCard
+            img="https://links.papareact.com/4cj"
+            title="The Greatest Outdoors"
+            description="Wishlists curated by Airbnb"
+            buttonText="Get Inspired"
+            />
         </section>
       </main>
+
+      <Footer/>
     </div>
   )
 }
