aa4f854c660ecaa1841074f18b046605b74050bf
Akib <akibahmed2011@gmail.com>
2022-04-01 02:39:59 +0600

    step 2 added

---
+++ components/Hosting Form/Step2Option.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Step2Option({id, name, description}) {
+  console.log(description)
+  return (
+    <div className='mx-auto max-w-md md:max-w-lg w-full'>
+              <input type="radio" name="radio" value={name} id={id} className='hidden peer' />
+              <label htmlFor={id} className="border-[1px] border-grey peer-checked:border-black peer-checked:border-2 rounded-xl pl-4 pr-3 py-6 md:py-4 flex flex-col justify-between items-start">
+                <div className="text-md md:text-lg font-semibold">{name}</div>
+                <div className="inline-block text-sm text-[#717171]">
+                  {description}
+                </div>
+              </label>
+    </div>
+  )}
+
+export default Step2Option;
--- pages/become-a-host/form.js
+++ pages/become-a-host/form.js
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Step1 from '../../components/Hosting Form/Step1';
+import Step2 from '../../components/Hosting Form/Step2';
 
 function form() {
+  const [page, setPage] = useState(1);
+  const [data, setData] = useState({
+    user: {},
+    profile: {},
+    settings: {}
+  });
+
+  const goNextPage = () => {
+    setPage((page) => page + 1);
+  }
+
+  const updateData = (type, newData) => {
+    setData((data) => {
+      return { ...data, [type]: newData };
+    });
+  }
+
   return (
     <div>
       {/* Header Button */}
-
+      <div className="hidden fixed right-0 md:visible md:flex py-8 mx-12 md:justify-end">
+          <button className="rounded-3xl px-4 py-2 text-black bg-neutral-200 text-xs font-semibold hover:bg-neutral-400">
+            Exit
+          </button>
+      </div>
       {/* Question Component */}
-      <Step1 />
-
+      {page === 1 && <Step2 />}
       {/* Progress Bar and Buttons*/}
       <div className="w-full bg-white md:w-[50%] fixed bottom-0 md:right-0">
         <div className="w-full h-[2px] bg-[#EBEBEB]">
-          <div className="w-[10%] h-[2px] bg-black" />
+          <div className="w-[10%] h-[2px] bg-black"/>
         </div>
 
         {/* Buttons */}
