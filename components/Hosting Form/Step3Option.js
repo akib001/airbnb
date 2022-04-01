b61c2db02ee5e4787dd28f065abfe5424993955f
Akib <akibahmed2011@gmail.com>
2022-04-02 02:40:04 +0600

    stepOptions border problem fixed

--- components/Hosting Form/Step3Option.js
+++ components/Hosting Form/Step3Option.js
@@ -4,7 +4,7 @@ function Step3Option({id, name}) {
   return (
     <div className='mx-auto max-w-md md:max-w-lg w-full'>
               <input type="radio" name="radio" value={name} id={id} className='hidden peer' />
-              <label htmlFor={id} className="border-[1px] border-grey peer-checked:border-black peer-checked:border-2 rounded-xl pl-4 pr-3 py-6 md:py-8 md:pr-7 flex flex-col justify-between items-start">
+              <label htmlFor={id} className="border-[1px] peer-checked:border-black peer-checked:border-2 peer-checked:m-[-1px] hover:border-black hover:border-2 hover:m-[-1px] transition-all rounded-xl pl-4 pr-3 py-6 md:py-8 md:pr-7 flex flex-col justify-between items-start">
                 <div className="text-md md:text-lg md:text font-semibold">{name}</div>
               </label>
     </div>
